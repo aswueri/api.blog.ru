f7b4c23d1da89d33dc2fd99a7c8a4e2b644a1e9d
Almira <limalmiras@gmail.com>
2026-03-30 13:22:17 +0400

    ADD and UPDATE

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+require 'connectDb.php';
+require 'functions.php';
+//die(print_r($_POST));
+
+header('Content-Type: application/json');
+$method= $_SERVER['REQUEST_METHOD'];
+
+$params = explode( '/' , $_GET['q']);
+$type = $params[0];
+if (isset($params[1])){
+    $id = $params[1];
+}
+
+switch ($method)
+{
+    case 'GET':
+        if ($type==='posts') {
+            if(isset($id)){
+                getPost($pdo, $id);
+            }else{
+                getPosts($pdo);
+            }
+        }
+        break;
+    case 'POST':
+        if ($type==='posts') {
+            addPost($pdo, $_POST);
+        }
+        break;
+
+    case 'PATCH':
+        if ($type ==='posts') {
+            if(isset($id)){
+                $data = file_get_contents('php://input');
+                $data = json_decode($data, true);
+                die($data['title']);
+                    UpdatePost($pdo, $id, $data);
+            }
+        }
+        break;
+
+
+}
