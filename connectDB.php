42065754556a6517c9c488f99c54fe9f6be9f237
Almira <limalmiras@gmail.com>
2026-04-01 12:03:00 +0400

    Завершенный проект

--- connectDB.php
+++ connectDB.php
@@ -1,9 +1,3 @@
 <?php
 
-// try {
     $pdo = new PDO("mysql:host=localhost;dbname=api test", "root", "");
-//     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// } catch (PDOException $e) {
-//     http_response_code(500);
-//     echo json_encode(['error' => 'Database connection failed: ' . $e->getMessage()]);
-//      exit;
